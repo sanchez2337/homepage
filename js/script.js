3e3014a4c92ef6f271733a931a3c20d3904eb2af
Adam Michałowski <michaowskiadam@gmail.com>
2022-10-23 10:01:13 +0200

    Functions

--- js/script.js
+++ js/script.js
@@ -1,30 +1,38 @@
-console.log("Witam wszystkich. Ten kod jest już w repozytorium Git");
+{
+    console.log("Witam wszystkich.");
 
-let day = document.querySelector(".js-day").innerText;
-let topic = document.querySelector(".js-topic");
+    const toggleBackground = () => {
+        let currentBackground = document.querySelector(".js-currentBackground");
+        let body = document.querySelector(".body");
+        body.classList.toggle("dark");
+        currentBackground.innerText = body.classList.contains("dark") ? "jaśniejsze" : "ciemniejsze";
+    }
 
-let body = document.querySelector(".body");
-let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
-let currentBackground = document.querySelector(".js-currentBackground");
+    const init = () => {
+        let changeBackgroundButton = document.querySelector(".js-changeBackgroundButton");
 
-switch (day) {
-    case "poniedziałek":
-    case "środa":
-    case "piątek":
-    case "niedziela":
-        topic.innerText = "programowania";
-        break;
-    case "wtorek":
-        topic.innerText = "ekonomii";
-        break;
-    case "czwartek":
-        topic.innerText = "inwestowania";
-        break;
-    default:
-        topic.innerText = "sportu";
+        changeBackgroundButton.addEventListener("click", toggleBackground);
+    }
+
+    init ();
+
+    let day = document.querySelector(".js-day").innerText;
+    let topic = document.querySelector(".js-topic");
+
+    switch (day) {
+        case "poniedziałek":
+        case "środa":
+        case "piątek":
+        case "niedziela":
+            topic.innerText = "programowania";
+            break;
+        case "wtorek":
+            topic.innerText = "ekonomii";
+            break;
+        case "czwartek":
+            topic.innerText = "inwestowania";
+            break;
+        default:
+            topic.innerText = "sportu";
+    }
 }
-
-changeBackgroundButton.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    currentBackground.innerText=body.classList.contains("dark") ? "jaśniejsze" : "ciemniejsze";
-});
